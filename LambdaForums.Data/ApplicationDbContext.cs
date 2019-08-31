6d599fd8a1b9bba969cd7a348012f39b23e97ded
Sriram <43503475+ScionOfIksvaku@users.noreply.github.com>
2019-08-31 09:59:47 -0500

    Initial Commit, Create data models

---
+++ LambdaForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using LambdaForums.Data.Models;
+
+namespace LambdaForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+
+
+    }
+}
